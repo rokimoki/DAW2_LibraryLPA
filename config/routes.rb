8cb2047ec2080e4ce44e3fc39367220887ee5625
Santiago Miguel Gubern González <migui1995@hotmail.es>
2018-04-23 00:06:53 +0100

    Added the need of authentication and new homePage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path:'', path_names:{ sign_in:'login', sign_out:'logout', sign_up:'register' }
-  root to: "books#index"
+  root to: "pages#index"
 
   resources :messages
   resources :chat_rooms
